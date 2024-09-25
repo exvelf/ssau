56bac2c7a9bee122de13e36f2020be66b20dd663
Jujia Exvelf <smetana.js@mail.ru>
2024-09-25 20:29:10 +0400

    Задание 5

--- src/main/java/MyFirstProgram.java
+++ src/main/java/MyFirstProgram.java
@@ -1,3 +1,5 @@
+import MyFirstPackage.*;
+
 class MyFirstClass {
     public static void main(String[] args){
         int i, j;
@@ -12,35 +14,5 @@ class MyFirstClass {
             }
             System.out.println();
         }
-    }
-}
-class MySecondClass {
-    private int param1, param2;
-    public int summ () {
-        return this.param1+this.param2;
-    }
-
-    public MySecondClass() {
-    }
-
-    public MySecondClass(int param1, int param2) {
-        this.param1 = param1;
-        this.param2 = param2;
-    }
-
-    public int getParam2() {
-        return param2;
-    }
-
-    public void setParam2(int param2) {
-        this.param2 = param2;
-    }
-
-    public int getParam1() {
-        return param1;
-    }
-
-    public void setParam1(int param1) {
-        this.param1 = param1;
     }
 }
