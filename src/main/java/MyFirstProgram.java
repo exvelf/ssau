84d5ebe30133caece0e43d9e145936b3d4009f71
Jujia Exvelf <smetana.js@mail.ru>
2024-09-25 20:04:45 +0400

    Задание 3

--- src/main/java/MyFirstProgram.java
+++ src/main/java/MyFirstProgram.java
@@ -1,5 +1,6 @@
 class MyFirstClass {
     public static void main(String[] args){
-        System.out.println("Hello world!!!");
+        for (int i = 0; i < args.length; i++)
+            System.out.println(args[i]);
     }
 }
